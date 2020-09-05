640925af33dbc298a07f87e05d42c8ab4ea5c3ba
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2020-09-05 23:20:25 +0900

    dataset works

--- model_trainer.py
+++ model_trainer.py
@@ -5,6 +5,9 @@ import time
 from custom_tqdm import TqdmNotebookCallback
 from tqdm.keras import TqdmCallback
 import albumentations as A
+import random
+from skimage import transform, util
+import cv2
 
 
 class AdiposeModel(keras.Model):
@@ -12,10 +15,17 @@ class AdiposeModel(keras.Model):
         """
         Because of numerical stability, softmax layer should be
         taken out, and use it only when not training.
-        Args
-            inputs : keras.Input
-            model_function : function that takes keras.Input and returns
-            output tensor of logits
+
+        arguments
+        ---------
+        inputs : keras.Input
+        model_function 
+            function that takes keras.Input and returns
+
+        output
+        ------
+            output : Tensor
+                tensor of logits
         """
         super().__init__()
         outputs = model_function(inputs)
@@ -29,21 +39,58 @@ class AdiposeModel(keras.Model):
         return tf.math.sigmoid(self.logits(inputs, training=training))
 
 class AugGenerator():
-    def __init__(self, X, Y):
-        self.X = X
-        self.Y = Y
-        self.n = self.X.shape[0]
+    """An iterable generator that makes data
+
+    NOTE: Every img is reshaped to img_size
+    NOTE: The position value is like pygame. (width, height),
+          which does not match with common image order (height,width)
+
+          Image input is expected to be the shape of (height, width),
+          i.e. the transformation to match two is handled in here automatically
+
+    return
+    ------
+    X : tuple of (img, pos), dtype= np.uint8, np.int32
+        img : np.array 
+        pos : tuple
+    Y : tuple of (xmin, ymin, xmax, ymax) dtype= np.int32
+    """
+    def __init__(self, img, data, img_size):
+        """ 
+        arguments
+        ---------
+        img : list
+            list of images, in the original size (height, width, 3)
+        data : list of dict
+            Each dict has :
+                'image' : index of the image. The index should match with img
+                'mask' : [rr, cc]
+                'box' : [[xmin, ymin], [xmax,ymax]]
+                'size' : the size of the image that the data was created with
+                        IMPORTANT : (WIDTH, HEIGHT)
+        img_size : tuple
+            Desired output image size
+            IMPORTANT : (HEIGHT, WIDTH)
+        """
+        self.img = img
+        self.data = data
+        self.n = len(data)
+        self.output_size = img_size
         self.aug = A.Compose([
             A.OneOf([
                 A.RandomGamma((40,200),p=1),
-                A.RandomBrightness(p=1),
-                A.RandomContrast(p=1),
+                A.RandomBrightness(limit=0.5, p=1),
+                A.RandomContrast(limit=0.5,p=1),
                 A.RGBShift(p=1),
             ], p=0.8),
             A.VerticalFlip(p=0.5),
-            A.RandomRotate90(p=1)
-        ])
-        self.idx = 0
+            A.RandomRotate90(p=1),
+            A.Resize(img_size[0], img_size[1]),
+        ],
+        bbox_params=A.BboxParams(format='pascal_voc', label_fields=['bbox_classes']),
+        keypoint_params=A.KeypointParams(format='xy'),
+        )
+        self.resize = A.Resize(img_size[0], img_size[1])
 
     def __iter__(self):
         return self
@@ -52,23 +99,66 @@ class AugGenerator():
         return self
 
     def __next__(self):
-        distorted = self.aug(
-            image=self.X[self.idx],
-            mask=self.Y[self.idx],
-        )
-        self.idx += 1
-        self.idx = self.idx % self.n
-        return distorted['image'], distorted['mask']
+        idx = random.randrange(0,self.n)
+        datum = self.data[idx]
+        image = self.img[datum['image']].copy()
 
-def create_train_dataset(X, Y, batch_size):
+        mask_idx = random.randrange(0,len(datum['mask'][0]))
+        pos = (datum['mask'][0][mask_idx], datum['mask'][1][mask_idx])
+
+        box_min = datum['box'][0]
+        box_max = datum['box'][1]
+
+        if np.any(np.not_equal(image.shape[:2], np.flip(datum['size']))):
+            row_ratio = image.shape[0] / datum['size'][1]
+            col_ratio = image.shape[1] / datum['size'][0]
+            pos = np.multiply(pos, [col_ratio,row_ratio]).astype(np.int32)
+            box_min = np.multiply(box_min, [col_ratio,row_ratio]).astype(np.int32)
+            box_max = np.multiply(box_max, [col_ratio,row_ratio]).astype(np.int32)
+        
+        if np.all(np.greater(box_min,[0,0])):
+            crop_min = np.random.randint([0,0],np.flip(box_min))
+        else :
+            crop_min = [0,0]
+
+        if np.all(np.less(box_max, image.shape[:2])):
+            crop_max = np.random.randint(np.flip(box_max), image.shape[:2])
+        else :
+            crop_max = image.shape[:2]
+
+        cropped_image = image[crop_min[0]:crop_max[0],crop_min[1]:crop_max[1]]
+        cropped_box_min = np.subtract(box_min, np.flip(crop_min))
+        cropped_box_max = np.subtract(box_max, np.flip(crop_min))
+        cropped_box = np.append(cropped_box_min, cropped_box_max)
+        cropped_pos = np.subtract(pos, np.flip(crop_min))
+        transformed = self.aug(
+            image=cropped_image,
+            bboxes=[cropped_box],
+            bbox_classes=['cell'],
+            keypoints=[cropped_pos],
+        )
+        t_img = np.array(transformed['image'],np.uint8)
+        t_pos = np.array(transformed['keypoints'][0], np.int32)
+        t_box = np.array(transformed['bboxes'][0], np.int32)
+        X = (t_img, t_pos)
+        Y = t_box
+        return X, Y
+
+
+def create_train_dataset(img, data, img_size, batch_size):
     autotune = tf.data.experimental.AUTOTUNE
     dataset = tf.data.Dataset.from_generator(
-        AugGenerator,
-        (tf.uint8, tf.float32),
-        (tf.TensorShape(X.shape[1:]), tf.TensorShape(Y.shape[1:])),
-        args = [X,Y],
+        AugGenerator(img, data, img_size),
+        ((tf.uint8, tf.int32), tf.int32),
+        output_shapes=(
+            (
+                tf.TensorShape([img_size[0],img_size[1],3]),
+                tf.TensorShape([2])
+            ),
+            tf.TensorShape([4])
+        )
     )
-    dataset = dataset.shuffle(X.shape[0])
+    dataset = dataset.shuffle(min(len(data),1000))
     dataset = dataset.batch(batch_size, drop_remainder=True)
     dataset = dataset.prefetch(autotune)
     dataset = dataset.repeat()
@@ -184,17 +274,62 @@ def run_training(
     print('Took {} seconds'.format(time.time()-st))
 
 if __name__ == '__main__':
+    import os
+    import imageio as io
+    import json
     import numpy as np
     import matplotlib.pyplot as plt
-    with np.load('cell_mask_data.npz') as data:
-        X = data['img']
-        Y = data['mask']
-    ds = create_train_dataset(X,Y)
+    from skimage import draw
+    import cv2
+    img_names = os.listdir('data/done')
+    img = []
+    img_name_dict = {}
+    for idx, name in enumerate(img_names):
+        img.append(io.imread('data/done/'+name))
+        img_name_dict[name] = idx
+
+    json_names = os.listdir('data/save')
+    data = []
+    for name in json_names:
+        with open('data/save/'+name,'r') as j:
+            data.extend(json.load(j))
+    for datum in data :
+        datum['image'] = img_name_dict[datum['image']]
+
+    # fig = plt.figure()
+    # d_idx = random.randrange(0,len(data)-5)
+    # for i, d in enumerate(data[d_idx:d_idx+5]):
+    #     image = img[d['image']].copy()
+    #     image = cv2.resize(image, (1200,900), interpolation=cv2.INTER_LINEAR)
+    #     mask = d['mask']
+    #     m_idx = random.randrange(0,len(mask[0]))
+    #     pos = (mask[0][m_idx], mask[1][m_idx])
+    #     boxmin = d['box'][0]
+    #     boxmax = d['box'][1]
+    #     rr, cc = draw.disk((pos[1],pos[0]),5)
+    #     image[rr, cc] = [0,255,0]
+    #     rr, cc = draw.rectangle_perimeter((boxmin[1],boxmin[0]),(boxmax[1],boxmax[0]))
+    #     image[rr,cc] = [255,0,0]
+    #     image[mask[1],mask[0]] = [100,100,100]
+    #     ax = fig.add_subplot(5,1,i+1)
+    #     ax.imshow(image)
+    # plt.show()
+
+    # gen = AugGenerator(img, data, (400,400))
+    # s = next(gen)
+
+    ds = create_train_dataset(img, data, (300,400), 1)
+    sample = ds.take(5).as_numpy_iterator()
     fig = plt.figure()
-    for image, mask in ds.take(1):
-        for idx in range(6):
-            ax = fig.add_subplot(6,2,2*idx+1)
-            ax.imshow(image[idx])
-            ax = fig.add_subplot(6,2,2*idx+2)
-            ax.imshow(mask[idx])
+    for i, s in enumerate(sample):
+        ax = fig.add_subplot(5,1,i+1)
+        img, pos = s[0]
+        img = img[0]
+        pos = pos[0]
+        xmin,ymin,xmax,ymax = s[1][0]
+        rr, cc = draw.disk((pos[1],pos[0]),5)
+        img[rr,cc] = [0,255,0]
+        rr, cc = draw.rectangle_perimeter((ymin,xmin),(ymax,xmax))
+        img[rr,cc] = [255,0,0]
+        ax.imshow(img)
     plt.show()

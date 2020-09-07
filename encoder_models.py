650a9b83b22e3d9a5d7d2d2ef94cd794de210f80
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2020-09-07 15:31:29 +0900

    double filter

--- encoder_models.py
+++ encoder_models.py
@@ -120,3 +120,70 @@ def hr_5_3_0_gap(inputs):
     x = layers.GlobalAvgPool2D()(x)
     outputs = layers.Activation('linear', dtype='float32')(x)
     return outputs
+
+def hr_5_3_16_gap(inputs):
+    x = [inputs]
+    x = clayers.HighResolutionModule(
+        filters=[16],
+        blocks=[3],
+        name='HR_0'
+    )(x)
+    x = clayers.HighResolutionModule(
+        filters=[16,32],
+        blocks=[3,3],
+        name='HR_1'
+    )(x)
+    x = clayers.HighResolutionModule(
+        filters=[16,32,64],
+        blocks=[3,3,3],
+        name='HR_2'
+    )(x)
+    x = clayers.HighResolutionModule(
+        filters=[16,32,64,128],
+        blocks=[3,3,3,3],
+        name='HR_3'
+    )(x)
+    x = clayers.HighResolutionModule(
+        filters=[16,32,64,128],
+        blocks=[3,3,3,3],
+        name='HR_4'
+    )(x)
+    x_16_to_32 = clayers.BasicBlock(
+        filters=32,
+        stride=2,
+        name='Downsample_0'
+    )(x[0])
+    x_32 = layers.Add()([x_16_to_32, x[1]])
+    x_32 = layers.BatchNormalization(momentum=clayers.BN_MOMENTUM)(x_32)
+    x_32 = layers.ReLU()(x_32)
+    x_32_to_64 = clayers.BasicBlock(
+        filters=64,
+        stride=2,
+        name='Downsample_1'
+    )(x_32)
+    x_64 = layers.Add()([x_32_to_64, x[2]])
+    x_64 = layers.BatchNormalization(momentum=clayers.BN_MOMENTUM)(x_64)
+    x_64 = layers.ReLU()(x_64)
+    x_64_to_128 = clayers.BasicBlock(
+        filters=128,
+        stride=2,
+        name='Downsample_2'
+    )(x_64)
+    x_128 = layers.Add()([x_64_to_128,x[3]])
+    x_128 = layers.BatchNormalization(momentum=clayers.BN_MOMENTUM)(x_128)
+    x_128 = layers.ReLU()(x_128)
+    x_256 = clayers.BasicBlock(
+        filters=256,
+        stride=2,
+        name='Downsample_3'
+    )(x_128)
+    x = layers.Conv2D(
+        1024,
+        1,
+        padding='same',
+    )(x_256)
+    x = layers.BatchNormalization(momentum=clayers.BN_MOMENTUM)(x)
+    x = layers.ReLU()(x)
+    x = layers.GlobalAvgPool2D()(x)
+    outputs = layers.Activation('linear', dtype='float32')(x)
+    return outputs
